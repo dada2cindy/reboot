ec09b850ce96b1c69fbfc58ddd8aa569f31fcea7
dada2cindy <dada2cindy@gmail.com>
2016-04-04 21:51:08 +0800

    update

---
+++ Reboot/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Reboot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnReboot = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNeedRebootTimes = new System.Windows.Forms.Label();
+            this.btnRebootM = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnReboot
+            // 
+            this.btnReboot.Location = new System.Drawing.Point(12, 88);
+            this.btnReboot.Name = "btnReboot";
+            this.btnReboot.Size = new System.Drawing.Size(75, 23);
+            this.btnReboot.TabIndex = 1;
+            this.btnReboot.Text = "重新啟動(1)";
+            this.btnReboot.UseVisualStyleBackColor = true;
+            this.btnReboot.Click += new System.EventHandler(this.btnReboot_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "剩餘開機次數：";
+            // 
+            // lblNeedRebootTimes
+            // 
+            this.lblNeedRebootTimes.AutoSize = true;
+            this.lblNeedRebootTimes.Location = new System.Drawing.Point(218, 25);
+            this.lblNeedRebootTimes.Name = "lblNeedRebootTimes";
+            this.lblNeedRebootTimes.Size = new System.Drawing.Size(17, 12);
+            this.lblNeedRebootTimes.TabIndex = 3;
+            this.lblNeedRebootTimes.Text = "50";
+            // 
+            // btnRebootM
+            // 
+            this.btnRebootM.Location = new System.Drawing.Point(124, 88);
+            this.btnRebootM.Name = "btnRebootM";
+            this.btnRebootM.Size = new System.Drawing.Size(75, 23);
+            this.btnRebootM.TabIndex = 4;
+            this.btnRebootM.Text = "多次重啟";
+            this.btnRebootM.UseVisualStyleBackColor = true;
+            this.btnRebootM.Click += new System.EventHandler(this.btnRebootM_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Location = new System.Drawing.Point(303, 88);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(216, 88);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "暫停";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "倒數(秒)：";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(220, 51);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(11, 12);
+            this.lblTime.TabIndex = 8;
+            this.lblTime.Text = "5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 9;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 144);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRebootM);
+            this.Controls.Add(this.lblNeedRebootTimes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReboot);
+            this.Name = "Form1";
+            this.Text = "WuDADA-重新開機工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReboot;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNeedRebootTimes;
+        private System.Windows.Forms.Button btnRebootM;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Reboot/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+
+namespace Reboot
+{
+    public partial class Form1 : Form
+    {
+        readonly int NEED_REBOOT_TIMES = 50;
+        readonly int REBOOT_TIME = 5;
+        private static readonly string CONFIG_FILE = "config.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDataToUI();
+        }
+
+        private void LoadDataToUI()
+        {
+            StreamReader stmRdr = new StreamReader(CONFIG_FILE);
+            string needRebootTimes = "";
+            string start = "";
+            string rebootTime = "";
+
+            string line = stmRdr.ReadLine();
+            while (line != null)
+            {
+                line = line.Trim();
+                if (line.IndexOf("NeedRebootTimes=") != -1)
+                {
+                    needRebootTimes = line.Replace("NeedRebootTimes=", "");
+                }
+                else if (line.IndexOf("Start=") != -1)
+                {
+                    start = line.Replace("Start=", "");
+                }
+                else if (line.IndexOf("RebootTime=") != -1)
+                {
+                    rebootTime = line.Replace("RebootTime=", "");
+                }
+
+                line = stmRdr.ReadLine();
+            }
+
+            stmRdr.Close();
+            stmRdr.Dispose();
+
+            lblNeedRebootTimes.Text = needRebootTimes;
+            lblTime.Text = rebootTime;
+            
+            if (bool.Parse(start) && int.Parse(needRebootTimes) > 0)
+            {
+                btnRebootM.Enabled = false;
+                btnStop.Enabled = true;
+                btnCancel.Enabled = true;
+                timer1.Enabled = true;
+            }
+        }
+
+        private void btnReboot_Click(object sender, EventArgs e)
+        {
+            if (timer2.Enabled)
+            {
+                lblTime.Text = REBOOT_TIME.ToString();
+                timer2.Enabled = false;
+                btnReboot.Text = "重新啟動(1)";
+            }
+            else
+            {
+                lblTime.Text = REBOOT_TIME.ToString();
+                timer2.Enabled = true;
+                btnReboot.Text = "取消(1)";
+            }
+        }
+
+        private static void DoReboot()
+        {
+            ManagementClass mc = new ManagementClass("Win32_OperatingSystem");
+
+
+            mc.Scope.Options.EnablePrivileges = true;
+
+
+            foreach (ManagementObject mo in mc.GetInstances())
+            {
+
+                mo.InvokeMethod("Reboot", null, null);
+
+            }
+
+
+            mc.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int minute = int.Parse(lblTime.Text);
+
+            if (minute == 0)
+            {
+                timer1.Enabled = false;
+                StreamReader stmRdr = new StreamReader(CONFIG_FILE);
+                string needRebootTimes = "";
+                string start = "";
+                string rebootTime = "";
+
+                string line = stmRdr.ReadLine();
+                while (line != null)
+                {
+                    line = line.Trim();
+                    if (line.IndexOf("NeedRebootTimes=") != -1)
+                    {
+                        needRebootTimes = line.Replace("NeedRebootTimes=", "");
+                    }
+                    else if (line.IndexOf("Start=") != -1)
+                    {
+                        start = line.Replace("Start=", "");
+                    }
+                    else if (line.IndexOf("RebootTime=") != -1)
+                    {
+                        rebootTime = line.Replace("RebootTime=", "");
+                    }
+
+                    line = stmRdr.ReadLine();
+                }
+
+                stmRdr.Close();
+                stmRdr.Dispose();
+
+                if (!string.IsNullOrEmpty(start) && !string.IsNullOrEmpty(needRebootTimes) && !string.IsNullOrEmpty(rebootTime))
+                {
+                    if (bool.Parse(start) && int.Parse(needRebootTimes) > 0 && int.Parse(rebootTime) > 0)
+                    {
+                        UpdateConfig(bool.Parse(start), (int.Parse(needRebootTimes) - 1), int.Parse(rebootTime));
+                        DoReboot();
+                    }
+                }
+            }
+            else
+            {
+                lblTime.Text = (minute - 1).ToString();
+            }
+        }
+
+        private void UpdateConfig(bool start, int rebootTimes, int rebootTime)
+        {
+            StreamWriter stmWdr = new StreamWriter(CONFIG_FILE, false);
+
+            stmWdr.WriteLine(string.Format("NeedRebootTimes={0}", rebootTimes));
+            stmWdr.WriteLine(string.Format("Start={0}", start));
+            stmWdr.WriteLine(string.Format("RebootTime={0}", rebootTime));
+
+            stmWdr.Close();
+            stmWdr.Dispose();
+        }
+
+        private void btnRebootM_Click(object sender, EventArgs e)
+        {
+            btnRebootM.Enabled = false;
+            btnStop.Enabled = true;
+            btnCancel.Enabled = true;
+            UpdateConfig(true, int.Parse(lblNeedRebootTimes.Text), int.Parse(lblTime.Text));
+            timer1.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnRebootM.Enabled = true;
+            btnStop.Enabled = false;
+            btnCancel.Enabled = true;
+            timer1.Enabled = false;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //重設
+            DoReset();
+        }
+
+        private void DoReset()
+        {
+            btnRebootM.Enabled = true;
+            btnStop.Enabled = false;
+            btnCancel.Enabled = false;
+            timer1.Enabled = false;
+
+            lblNeedRebootTimes.Text = NEED_REBOOT_TIMES.ToString();
+            lblTime.Text = REBOOT_TIME.ToString();
+
+            UpdateConfig(false, NEED_REBOOT_TIMES, REBOOT_TIME);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int minute = int.Parse(lblTime.Text);
+
+            if (minute == 0)
+            {
+                DoReboot();
+            }
+            else
+            {
+                lblTime.Text = (minute - 1).ToString();
+            }
+        }
+    }
+}
